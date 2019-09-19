8291bd29d0b7234a426a173f64b3666f728b6be7
Cam Nugent <nugentc@uoguelph.ca>
2019-09-19 14:26:48 -0400

    improving adj_seq algorithms

--- R/WIP.r
+++ R/WIP.r
@@ -25,9 +25,21 @@
 
 
 
+#DONE - need to stop masking for triple deletes, these are valid missing codons.
+#TODO -  adjust seq seems to be dropping the final bp... is this because its being lost in the frame step?
+change in adjust seq from:
+  #this prevents additions beyond the end of the original sequence.
+  if(org_seq_pos >= length(org_seq_vec)){
+    break
+  }
 
+to:
+  #this prevents additions beyond the end of the original sequence.
+  if(org_seq_pos > length(org_seq_vec)){
+    break
+  }
+#^fix this in coiDenoiser as well.
 
-#TODO - need to stop masking for triple deletes, these are valid missing codons.
 #TODO - don't need to add the information at the start of the adjusted sequence.
 #TODO - use frame to just store the start and end positions in three bits:
 #  $front
@@ -41,4 +53,41 @@
 # maybe will be faster if we don't touch the characters repeatedly but instead just manipulate a vector via index positions.
 # TODO - less important than making it work but will have to rewrite the documentation to account for the new data
 # structures being used here
+# TODO - sujeevan said that some of the reads may be the reverse compliments? figure out how these are handled... can it be fixed?
+
+
+
+#TODO - move the new adj_seq  + triple_in functions into coiDenoiser... add these uit tests as well.
+path_test1 = c(1,1,2,1,1,1,1,1,0,0,0,1,0,1,1,1,1) #return c(9,10,11)
+path_test2 = c(1,1,2,1,1,1,1,1,0,0,1,0,1,1,1,1) #return null
+path_test3 = c(1,1,2,1,1,1,1,1,0,0,0,1,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0) #return c(9,10,11,21,22,23)
+
+triple_ins(path_test1, 1, length(path_test1))
+triple_ins(path_test2, 1,length(path_test2))
+triple_ins(path_test3, 1,(length(path_test3)-5))
+
+
+
+x= DNAseq(example_nt_string)
+x
+x = frame(x)
+x$data
+x = adjust(x, censor_length = 0)
+x$adjusted_sequence
+
+
+
+
+
+err = "ctctacttgatttttggtgcatgagcaggaatagttggaatagctttaagtttactaattcgcgctgaactaggtcaacccggatctcttttagggggatgatcagatttataatgtgatcgtaaccgcccatgcctttgtaataatcttttttatggttatacctgtaataattggtggctttggcaattgacttgttcctttaataattggtgcaccagatatagcattccctcgaataaataatataagtttctggcttcttcctccttcgttcttacttctcctggcctccgcaggagtagaagctggagcaggaaccggatgaactgtatatcctcctttagcaggtaatttagcacatgctggcccctctgttgatttagccatcttttcccttcatttggccggtatctcatcaattttagcctctattaattttattacaactattattaatataaaacccccaactatttctcaatatcaaacaccattatttgtttgatctattcttatcaccactgttcttctactccttgctctccctgttcttgcagccggaattacaatattattaacagaccgcaacctcaacactacattctttgaccccgcagggggaggggacccaattctctatcaacactta"
+
+y= DNAseq(err)
+y
+y = frame(y)
+y$data
+y = adjust(y, censor_length = 3)
+y$adjusted_sequence #looks good but the last character is dropped!
+
+
+
 
