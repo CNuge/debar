777f68fcc73eb0f51b35f8af0ca7e67357a2e44c
Cam Nugent <nugentc@uoguelph.ca>
2019-09-24 11:39:14 -0400

    todo next steps

--- R/WIP.r
+++ R/WIP.r
@@ -14,19 +14,18 @@
 #    will be more correct.
 
 
-
 #this is branched off from coiDenoiser
 # a minimal use case of only denoising one sequence at a time - no need to take the consensus 
 # or to act on multiple sequences individually.
 
-#changing the object so that it is just a single DNA sequence
+#cDONE - hanging the object so that it is just a single DNA sequence
 #using the small PHMM (10K training) that is generic for all the different data... can sub in the higher trained 
 #order specific later.
 
-
-
 #DONE - need to stop masking for triple deletes, these are valid missing codons.
-#TODO -  adjust seq seems to be dropping the final bp... is this because its being lost in the frame step?
+#DONE -  adjust seq seems to be dropping the final bp... is this because its being lost in the frame step?
+#DONE - don't need to add the information at the start of the adjusted sequence.
+
 change in adjust seq from:
   #this prevents additions beyond the end of the original sequence.
   if(org_seq_pos >= length(org_seq_vec)){
@@ -40,44 +39,15 @@ to:
   }
 #^fix this in coiDenoiser as well.
 
-#TODO - don't need to add the information at the start of the adjusted sequence.
-#TODO - use frame to just store the start and end positions in three bits:
-#  $front
-#  $framed
-#  $end
-# don't want to toss the excess information, (may need to change the double frame logic)
-# instead we want this put to the side so it can be patched back on the sequence after the middle
-# is denoised
-# TODO - AA check with the PHMM, project the masks back down a level corresponding to the first 0 or 2 seen.
+
+# TODO - housecleaing on the data structure, make sure the program is not saving unneecessary steps in memory
+# TODO - make the reapplication of the ends an option on the io, default = TRUE
+# can make it false and then just output the adjusted seq directly instead of the patch job that is done currently
 # TODO - rejig so that instead of making additional strings, just saving index positions on the initial raw sequence.
 # maybe will be faster if we don't touch the characters repeatedly but instead just manipulate a vector via index positions.
-# TODO - less important than making it work but will have to rewrite the documentation to account for the new data
-# structures being used herem frame_offset The offset to the reading frame to be applied for translation. By default the offset
-#' is zero, so the first character in the framed sequence is considered the first nucelotide of the first codon.
-#' Passing frame_offset = 1 would offset the sequence by one and therefore make the second character in the
-#' framed sequence the the first nucelotide of the first codon.
-#' @return a class object of code{"ccs_reads"} 
-#' @seealso \code{\link{build_ccs}}
-#' @seealso \code{\link{frame}}
-#' @seealso \code{\link{adjust}}
-#' @examples
-#' #previously called
-#' ex_data = build_ccs(ex_ccs_read_list, order = 'Diptera', id = 'SSGBC787-14')
-#' ex_data =  frame(ex_data)
-#' ex_data = adjust(ex_data)
-#' ex_data = consensus(ex_data)
-#' #run the aa check on the adjusted sequences
-#' ex_data = aa_check(ex_data)
-#' @export
-#' @name aa_check
-aa_check = function(x, ...){
-  UseMethod("aa_check")
-} 
-
-#' @rdname aa_check
-#' @export
-
 # TODO - sujeevan said that some of the reads may be the reverse compliments? figure out how these are handled... can it be fixed?
+# TODO - less important than making it work but will have to rewrite the documentation to account for the new data
+# structures being used 
 
 
 
@@ -90,7 +60,21 @@ triple_ins(path_test1, 1, length(path_test1))
 triple_ins(path_test2, 1,length(path_test2))
 triple_ins(path_test3, 1,(length(path_test3)-5))
 
+#DONE - use frame to just store the start and end positions in three bits:
+#  $front
+#  $framed
+#  $end
+# don't want to toss the excess information, (may need to change the double frame logic)
+# instead we want this put to the side so it can be patched back on the sequence after the middle
+# is denoised
+# DONE - AA check with the PHMM, project the masks back down a level corresponding to the first 0 or 2 seen.
+# - problem is that the masks create a bunch of 0s for the aaPHMM... best to mask off just the aa sequence
+# DONE - save the trimmed front and end of the sequence and reapply after correction.
 
+
+###########
+# examples
+##########
 
 x= DNAseq(example_nt_string)
 x
@@ -146,7 +130,7 @@ x$frame_dat$framed_seq
 
 patched = c(x$data$raw_removed_front,
             x$frame_dat$removed_lead,
-            x$frame_dat$trimmed_seq[4:length(x$frame_dat$trimmed_seq)], #need to save the total number of dashes...
+            x$frame_dat$trimmed_seq[(x$data$len_first_front+1) :length(x$frame_dat$trimmed_seq)], #need to save the total number of dashes...
             x$frame_dat$removed_end,
             x$data$raw_removed_end)
 split_dat = strsplit(tolower(err_sides), "")[[1]]
@@ -163,6 +147,31 @@ write_fasta(x)
 write_fastq(x)
 
 
+
+err_sides = "AAAAAAAAAAAAAAAActctacttgatttttggtgcatgagcaggaatagttggaatagctttaagtttactaatttcgcgctgaactaggtcaacccggatctcttttaggggatgatcagatttataatgtgatcgtaaccgcccatgcctttgtaataatcttttttatggttatacctgtaataattggtggctttggcaattgacttgttcctttaataattggtgcaccagatatagcattccctcgaataaataatataagtttctggcttcttcctccttcgttcttacttctcctggcctccgcaggagtagaagctggagcaggaaccggatgaactgtatatcctcctttagcaggtaatttagcacatgctggcccctctgttgatttagccatcttttcccttcatttggccggtatctcatcaattttagcctctattaattttattacaactattattaatataaaacccccaactatttctcaatatcaaacaccattatttgtttgatctattcttatcaccactgttcttctactccttgctctccctgttcttgcagccggaattacaatattattaacagaccgcaacctcaacactacattctttgaccccgcagggggaggggacccaattctctatcaacacttaTTTTTTTTTT"
+nchar(err_big)
+x = DNAseq(err_sides, name = 'example_seq2' )
+x
+x = frame(x)
+x = adjust(x, censor_length = 3)
+x$adjusted_sequence 
+x
+x = aa_check(x)
+
+write_fasta(x)
+write_fastq(x)
+
+
+x = DNAseq(example_nt_string, name = 'example_seq1' )
+x
+x = frame(x)
+x = adjust(x, censor_length = 3)
+x$adjusted_sequence 
+x
+x = aa_check(x)
+
+write_fasta(x)
+write_fastq(x)
 
 
 
