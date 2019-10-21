1e469790a13074b57531209f72f37e0086cbb9b6
Cam Nugent <nugentc@uoguelph.ca>
2019-10-21 12:17:34 -0400

    working on the unit tests

--- tests/testthat/test_arguments.r
+++ tests/testthat/test_arguments.r
@@ -7,12 +7,13 @@ data = read_fastq(fastq_gz_real_file)
 
   
 x = denoise(data$sequence[[71]], keep_phred = FALSE, to_file = FALSE)
-x
 
+#x[['adjustment_count']] ==  2
+expect_equal(x[['adjustment_count']], 2)
 
-
-x = denoise(data$sequence[[78]], phred = data$quality[[187]])
-x
+#test that the warning is thrown if different lengths in the phred and DNA sequences.
+expect_error(denoise(data$sequence[[78]], phred = data$quality[[187]], to_file = FALSE), 
+"The length of the input DNA sequence and phred scores must match.")
 
 
 })
--- tests/testthat/test_messy_data.r
+++ tests/testthat/test_messy_data.r
@@ -3,12 +3,10 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   ############################################################################
   # Real read test 1
   ############################################################################
-  #this has a strong run of barcode data, there is a single reported error in the sequence (deleted character)
-  # blast of the sequence indicates it is a missing t at the homopolymer tract. ~149-157
-  #the path appears to get the position right :)
+  #this has a strong run of barcode data, but appears to be chimeric/ a half fwd half back
+  #this one should be rejected
   
   t1 = "gctcctaaaattgaagaaactcctgctaaatgtaaagaaaaaattgctaaatctactgaagcaccaccatgagcaataatagaagataaaggtggataaacagttcatccagtaccagccccattttctactatacttctagccattaatagagtcaatgaaggcggtaataatcagaaactcatattatttattcgaggaaatgctatatccggagctcctaatattaatggaactaatcaatttccaaatcctccaattataattggtattactataaaaaaaattattacgaaagcatgggcagtgacaattacattataaatttgatcatcaccaattaatgctcctggatgtcctaattcagctcgaattaagattcttagagaagttcctactattccagctcatgctccaaatataaagtacaaagttccaatatctttatgattggttgactgtgacctgagtcagctacatgttcgactgcatagacagtgcgctgtctatgcagtcgaacatgtagctgactcaggtcacggtcaacaaatcataaagatattggaactttgtactttatatttggagcatgagctggaatagtaggaacttctctaagaatcttaattcgagctgaattaggacatccaggagcattaattggtgatgatcaaatttataatgtaattgtcactgcccatgctttcgtaataatttttttatagtaataccaattataattggaggatttggaaattgattagttccattaatattaggagctccagatatagcatttcctcgaataaataatatgagtctctgattattaccgccttcattgactctattaatggctagaagtatagtagaaaatggggctggtactggatgaactgtttatccacctttatcttctattattgctcatggtggtgcttcagtagatttagcaattttttctttacatttagcaggagtttcttcaattttaggagc"
-  t1a = rev_comp(t1)
   
   test_seqdenoise = DNAseq(t1, name = "test1")
   test_seqdenoise = frame(test_seqdenoise)
@@ -27,13 +25,13 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   # a large string of 1s or twos is encountered... consider how to proceed given that the later
   #example, t3 is benefitting from the trunction at this early point.
   t2 = "CGTATGTATGTCGCGCAGTCGAACATGTAGCTGACTCAGGTCACGGTCAACAAATCATAAAGATATTGGAACTTTATATTTTATTTTTGGAATTTGAGCAGGAATAGTAGGAACTTCTTTAAGTTTATTAATTCGACTTGAATTAAGAACAATAAAAAATTTAATTGGTAATGATCAAATTTATAATGTAATTGTTACAGCCCACGCATTTATTATAATTTTTTTTATAGTTATACCAATTTTAATTGGAGGATTTGGAAATTGATTAGTCCCTATTATATTAGGAGCCCCAGATATAGCTTTTCCACGAATAAATAATATAAGATTCTGAATATTACCCCCTTCTTTATCTTTACTATCAATTAGAAGAATAGTAGAAACTGGGACCGGAACAGGATGAACTGTTTACCCACCTCTTTCTTCTGTAATTGCACATACTGGATCTTCAGTAGATTTTTCAATTTTTTCTCTACATATTGCAGGAATTTCATCTATTTTAGGAGCAATTAATTTTATTTCTACAATAATAAATATAAAAATTGAAAAATCTACTGAAGATCCAGTATGTGCAATTACAGAAGAAAGAGGTGGGTAAACAGTTCATCCTGTTCCGGTCCCAGTTTCTACTATTCTTCTAATTAATAGTAAAGATAAAGAAGGGGGTAATATTCAGAATCTTATATTATTTATTCGTGGAAAAGCTATATCTGGGGCTCCTAATATAATAGGGACTAATCAATTTCCAAATCCTCCAATTAAAATTGGTATAACTATAAAAAAAAATTATAATAAATGCGTGGGCTGTAACAATTACATTATAAATTTGATCATTACCAATTAAATTTTTTATTGTTCTTAATTCAAGTCGAATTAATAAACTTAAAGAAGTTCCTACTATTCCTGCTCAAATTCCAAAAATAAAATATAAAGTTCCAATATCTTTATGATTGGTTGAATGTGACCTGAGTCAGCTACATGTTCGACTGCGCGACATACATACG"
-  t2a = rev_comp(t2)
   
   test_seqdenoise2 = DNAseq(t2, name = "test1")
   test_seqdenoise2 = frame(test_seqdenoise2)
   test_seqdenoise2 = adjust(test_seqdenoise2)
   test_seqdenoise2 = outseq(test_seqdenoise2)
   
+  #no corrections applied for this one
   #test_seqdenoise2$outseq == t2
   expect_equal(test_seqdenoise2$outseq, t2)
   
@@ -49,12 +47,10 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   ############################################################################
   # This one encounters a large run of ones and a large run of 0s about halfway through the barcode reg 
   
-  
   t3 = 'AGGTCAACAAATCATAAAGATATTGGAACATTATATTTTTTATTTGGAATTTGAGCAGGAATAATTGGAACATCATTAAGATTATTAATTCGAATAGAATTAGGAAATCCTGGATCCTTAATTGGAGATGACCAAATTTATAATAACTATTGTAAACAGCCCATGCATTTATTATAATTTTTTTTATAGTAATACCTATTATAATTGGAGGATTTGGAAATTGATTAATTCCTTTAATATTAGGTGCACCAGATATAGCATTCCCCCGAATAAATAATATAAGATTTTGATTATTACCCCCTTCCATTTTTCTTTTAATTTCAAGAAGAATCGTAGAAAAATGGAGCAGGAACAGGATGAACTGTTTACCCCCCCTTATCTTCTAACACCGCTCATAGAGGAAGATCCGTAGATTTAGCCATTTTTTCTCTTCATTTAGCTGGAATTTCCTCAATTCTAGGAGCAGTAAATTTTATTTCTACAGTAATTAATATACGAGCTAAAAAAATAATATTTGACCAAATACCCCTATTTATTTGAGCTGTAGCTATTACTGCATTATTATTATTATTATCATTACCAGTTTTAGCAGGAGCTATTACTATATTATTAACAGATCGAAATTTAAATACATCTTTTTTTGACCCAGCTGGAGGAGGAGACCCAATTTTATACCAACATTTATTTTGATTTTTTGGACACCCTGAAGTTTA'
 
-  
   #Below is behaving well despite initial doubt. The path encounters a large run of 0000s in the middle of the sequence and therefore terminates. Only possible improvement would be to add lots of Ns and pick up on the second run of 1s
-  
+  #possibly - could add a stop/reject condition if the adjust_seq length is less than the min_match?  
   test_seqdenoise3 = DNAseq(t3, name = "test3")
   test_seqdenoise3 = frame(test_seqdenoise3)
   # test_seqdenoise3$data$path
@@ -78,9 +74,8 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   #This one introduces an error into the output with a bunch of NAs
   #reverse compliment again the correct orientation
   t4 = 'GGTAGCTGTCAGAGTAGCTCGTGGATCACTTGTGCAAGCATCACATCGTAGTAAACTTCAGGGTGTCCAAAAAATCAAAATAAATGTTGATATAAAATAGGGTCTCCCCCACCAGCTGGGTCAAAAAATGAGGTATTTAAATTTCGATCAGTTAATAATATAGTAATTGTTCCAGCTAAAACTGGTAAAGATAATAATAAAAGAAATGCAGTATACCATCTGCTCAAACAAATAATGGTATTTGATCAAATGATAAATTATTTAAACGTATATTAATAATTGTAGAAATAAAATTAATAGCTCCTAAAATAGATGAAATCCCAGCTAAATGAAGAGAAAAAATAGCTAAATCAACAGATCTTCCTCCATGAGCAATATTAGAAGAAAGAGGAGGATAAACTGTTCATCCAGTTCCTGCTCCATTTTCTACAATTCTACTTGAAATTAATAAAGTTAATGATGGGGGTAGAAGTCAAAAACTTATATTATTTATTCGGGGGAAGCTATATCTGGGGCTCCTAATATTAATGGTACTAATCAATTACCAAAT'
-  t4a = rev_comp(t4)
   
-  test_seqdenoise4 = DNAseq(t4a, name = "test4")
+  test_seqdenoise4 = DNAseq(t4, name = "test4")
   test_seqdenoise4 = frame(test_seqdenoise4)
   # test_seqdenoise4$data$path
   # test_seqdenoise4$data$raw_removed_front
@@ -90,13 +85,17 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   # test_seqdenoise4$adjusted_sequence
   test_seqdenoise4 = outseq(test_seqdenoise4)
   
-  #should turn it into Ns, adjusted sequence is just the frame front
-  trimmed_and_framed4 = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
-  test_seqdenoise4_trimmed =  outseq(test_seqdenoise4, keep_flanks = FALSE)
- 
-  #test_seqdenoise4_trimmed$outseq == trimmed_and_framed4
-  expect_equal(test_seqdenoise4_trimmed$outseq, trimmed_and_framed4)
+  #test_seqdenoise4$adjustment_count ==2
+  expect_equal(test_seqdenoise4$adjustment_count, 2)
   
+
+  #This one is an instance where the reverse compliment is the sequence that matches the path
+  #therefore if we override the direction check, the sequence should be rejected
+  test_seqdenoise4_trimmed = DNAseq(t4, name = "test4")
+  test_seqdenoise4_trimmed = frame(test_seqdenoise4_trimmed, dir_check = FALSE)
+  
+  #test_seqdenoise4_trimmed$reject == TRUE
+  expect_equal(test_seqdenoise4_trimmed$reject, TRUE)
   
   
   ############################################################################
@@ -108,7 +107,7 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
 
   test_seqdenoise5 = DNAseq(t5, name = "test5")
   test_seqdenoise5 = frame(test_seqdenoise5)
-  #path has 3 inserts scattered throughout a string of ones, plus a triple delete. lots of missing front and added end.
+  #path has 4 inserts scattered throughout a string of ones, plus a triple delete. lots of missing front and added end.
   #see if moving the back to the front reveals a circle?
   #test_seqdenoise5$data$path
 
@@ -132,16 +131,15 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   
   #should turn it into Ns, adjusted sequence is just the frame front
   #has the front edge added to the sequence.
-  trimmed_and_framed4 = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNTTGACTAGTTCCTTTAAAATTAGGAGCCCCAGATATAGCATTTCCTCGGATAAATAATATAAGTTTTTGANNNNNNNNNNCTTCATTAACTTTACTTTTATCAAGCTCTATTGTAGAAAATGGANNNNNNNNNNGTTGAACTGTTTACCCTCCTTTATCTTCTGGGATTGCCCATGCAGGAGCTTCTGTTGATTTAGCTATTTTTTCTCTTCACTTAGCTGAAATTTCCTCAATTTTAGGAGCAGTAAATTTTATTACAACTGTAATTAATATACGGTCTAGAGGAATTACTTTAGATCGAATACCTTTATTTGTTTGATCAGTAGTAATTACTGCAATTTTATTACTTCTCTCNNNNNNNNNNCTAGCTGGAGCTATTACNNNNNNNNNNACAGACCGAAATTTAAATACCTCTTTTTTTGACCCAGCAGGGGGAGGGGACCCAATTCTTTATCAACACTTA"
+  trimmed_and_framed5 = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNTTGACTAGTTCCTTTAAAATTAGGAGCCCCAGATATAGCATTTCCTCGGATAAATAATATAAGTTTTTGANNNNNNNNNNCTTCATTAACTTTACTTTTATCAAGCTCTATTGTAGAAAATGGANNNNNNNNNNGTTGAACTGTTTACCCTCCTTTATCTTCTGGGATTGCCCATGCAGGAGCTTCTGTTGATTTAGCTATTTTTTCTCTTCACTTAGCTGAAATTTCCTCAATTTTAGGAGCAGTAAATTTTATTACAACTGTAATTAATATACGGTCTAGAGGAATTACTTTAGATCGAATACCTTTATTTGTTTGATCAGTAGTAATTACTGCAATTTTATTACTTCTCTCNNNNNNNNNNCTAGCTGGAGCTATTACNNNNNNNNNNACAGACCGAAATTTAAATACCTCTTTTTTTGACCCAGCAGGGGGAGGGGACCCAATTCTTTATCAACACTTA"
   test_seqdenoise5_trimmed =  outseq(test_seqdenoise5, keep_flanks = FALSE)
   
-  #test_seqdenoise5_trimmed$outseq == trimmed_and_framed4
+  #test_seqdenoise5_trimmed$outseq == trimmed_and_framed5
   expect_equal(test_seqdenoise5_trimmed$outseq, trimmed_and_framed4)
   
   #the trimmed output should be 654 in length, because of the triple delete in the path
-  #nchar(test_seqdenoise5_trimmed$outseq)== 654
+  #nchar(test_seqdenoise5_trimmed$outseq) == 654
   expect_equal(nchar(test_seqdenoise5_trimmed$outseq), 654)
-  
   
   
   ############################################################################
@@ -152,7 +150,7 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   t6 = "GGTAGCGATCAGCTGAGCGCGTGGATCACTTGTGCAAGCATCACATCGTAGTAAACTTCAGGATGACCAAAAAATCAAAATAAATGTTGATATAAAATAGGGTCTCCTCCTCCAGCAGGGTCAAAAAATTAAGTATTTAAATTTCGGTCTGTTAATAGTATAGTAATAGCTCCTGCTTAAACTGGTAAGGATAAAAGTAATAATAAAGCTGTAATAACAACTGATCAAACAAATAATGGTATTCGATCATATGAAATTCCATTAGATCGTATATTAATTACAGTAGTAATAAAATTTACTGCTCCTAAAATTCATGATTTTCATGAGATAAATATCACCTTATTCTCAATCCATTGTACCAACAAAAATTCTTTCCTATCTTCTATTCCTTCTAACTAATGCCGTGCAACTTGCATTTAAAAGACCTTAGGTCTTACAGGCGTATAGTTAAAATTATTATTATTATTATTATTAAAATAGATGATATACCTGCTTAATGAAGAGAAAAAATTGCTAAATCAACAGAGGCTCCTCTATGAGCAATTCTAGAAGAAAGAGGGGGATAAACTGTTCATCCTGTTCCAGCTCCAGTTTTCAACTATACTACTTATTAGTAATAAAGTTAAAGAAGGAGGTAATAGTCAAAACCTTATATTATTTATTCGAGGAAATGCTATATCTGGAGCTCCCAATATTAGAGGTACTAATCAATTTCCAAATCCTCCAATTATAATTGGTATTACCTATAAAAAAAATTATAACAAAAGCGTGAGCTGTAACAATTACATTATAAATTTGATCATCTCCAATCAAAGCTCCTGGATGACCAAGTTCTGCACGAATTAAAATTCTTAAAGAAGTTCCTACTATTCCAGCTCAAGTTCCATATAAAAAATATAATGTTCCAATATCTTTATGATTTGTTGAACTGTGACCTGAGTCAGCTACATGTTCGACTGCAGAGACTGCGACGAGACTACC"
   t6a = rev_comp(t6)
   #this one the reverse compliment is the better match, and appears to be an ~420 run of good read with a single indel.
-  #
+  #this one would also be solved by a minimum length on the adjust seq.
   
   test_seqdenoise6 = DNAseq(t6a, name = "test6")
   test_seqdenoise6 = frame(test_seqdenoise6)
@@ -176,11 +174,7 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   # Real read test 7
   ############################################################################
 
-  
-  library(seqinr)
-  t7a = c2s(rev(comp(s2c(t7))))
-  
-  #This one has decent phred scores but appears to be getting completely censored.
+    #This one has decent phred scores but appears to be getting completely censored.
   #^ is this one of the rev compliment reads? YES!
   t7 = 'GTGCGTGTGACTTGGATCACTTGTGCAAGCATCACATCGTAGTAAACTTCTGGGTGTCCAAAAAATCAAAATAAATGTTGATATAAAATTGGATCACCTCCTCCTGAAGGATCAAAAAATGAAGTATTTAAATTTCGATCAAATAATAATATTGTAATAGCTCCTGCTAATACAGGTAATGACAATAATAATAAAATAGCCGTTAATAATAATGATCAACAAAATAATCTTACTTTTTCTATTTTATATATTCTCATATTTAAAATAGTAGTAATAAAATTAATTGATGCTATAATTGAAGATATACCAGCAATATGCAATGAAAAGATTGATAAATCTACCGATGCTCCAGAATGAGATAAATTTATAGACAATGGTGGATATACAGTTCATCCTGTTCCAGAACCTATTCCAATAAATATTCTTGATATTAATAATATAATTCTTGGAGGTAATAATCAAAATCTTATATTATTTATTCGAGGAAATGCTATATCAGGAACCGATAATATTATAGGAACTAAAAAATTTCCAAAACCCCCTATTATTACTGGTATCACAAAAAAAAAAAATTATTACAAAAGCATGAATAGTTACAATAGCATTATAAATTTGATCATTTCCTAAAAATGAACCAGGATTTCCTAATTCTAAACGAATTATTATTCTTAATGATAATCCTATTATTCCTGCTCAAAATCCAAAAATAAAATATAAAATTCCAATATCTTTATGATTGGTTGAATGTGACCTGAGTCAGCTACATGTTCGACTGCTGCGTGAGAGAGAGACCTACCGTACATGTGCAGTCGAACATGTAGCTGACTCAGGTCACGTTCAACAAATCATAAAGTATAATGTTCCAATATCTTTATGATTTGTTGAACGTGACCTGAGTCAGCTACATGTTCGACTGCACATGTACATCAGCTCCTACC'
 
@@ -205,23 +199,5 @@ test_that("Messy SEQUEL sequences are processed as anticipated", {
   #test_seqdenoise7_trimmed$outseq == trimmed_and_framed7
   expect_equal(test_seqdenoise7_trimmed$outseq, trimmed_and_framed7)
   
-  
-  
-  
-  test_seqdenoise7a = DNAseq(t7a, name = "test7a")
-  test_seqdenoise7a = frame(test_seqdenoise7a)
-  test_seqdenoise7a$data$path
-  test_seqdenoise7a$data$raw_removed_front
-  test_seqdenoise7a$frame_dat
-
-  test_seqdenoise7a = adjust(test_seqdenoise7a)
-  test_seqdenoise7a$adjusted_sequence
-  test_seqdenoise7a = outseq(test_seqdenoise7a)
-
-  trimmed_and_framed7 =
-  test_seqdenoise7a_trimmed =  outseq(test_seqdenoise7a, keep_flanks = FALSE)
-
-  #test_seqdenoise7a_trimmed$outseq == trimmed_and_framed4
-  expect_equal(test_seqdenoise7a_trimmed$outseq, trimmed_and_framed7)
   
   })
