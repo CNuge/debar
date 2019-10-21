155ece4ef2eaf4cd4bb8511949887c0759033d37
Cam Nugent <nugentc@uoguelph.ca>
2019-10-21 13:34:47 -0400

    default behaviour changed

--- tests/testthat/test_io.r
+++ tests/testthat/test_io.r
@@ -3,17 +3,17 @@ test_that("Sequence files are read in properly.", {
   fastq_dat_file = system.file('extdata/ccs_subset.fastq', package = 'seqdenoise')
   data1 = read_fastq(fastq_dat_file)
   
-  #all.equal(dim(data1), c(50,2))
-  expect_equal(all.equal(dim(data1), c(50,2)), TRUE)
+  #all.equal(dim(data1), c(50,3))
+  expect_equal(all.equal(dim(data1), c(50,3)), TRUE)
   
   fastq_real_file = system.file('extdata/sequel_smrt_subset.fastq', package = 'seqdenoise')
-  data2 = read_fastq(fastq_real_file, keep_quality = TRUE )
+  data2 = read_fastq(fastq_real_file)
   
   #all.equal(dim(data2), c(250,3))
   expect_equal(all.equal(dim(data2), c(250,3)), TRUE)
   
   fastq_gz_real_file = system.file('extdata/sequel_smrt_subset.fastq.gz', package = 'seqdenoise')
-  data3 = read_fastq(fastq_gz_real_file)
+  data3 = read_fastq(fastq_gz_real_file, keep_quality = FALSE)
   
   #all.equal(dim(data3), c(250,2))
   expect_equal(all.equal(dim(data3), c(250,2)), TRUE)
